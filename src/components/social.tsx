ad7773334858bdea666336669e833b7b26d7e2df
izzudin26 <izzudin.ar26@gmail.com>
2024-08-28 21:38:51 +0700

    Updates

--- src/components/social.tsx
+++
@@ -1,9 +0,0 @@
-import { TSocial } from "../data/social";
-
-export default function SocialIcon({ icon, url }: TSocial) {
-  return (
-    <a href={url} target="_blank">
-      <img src={icon} className="h-8" />
-    </a>
-  );
-}
